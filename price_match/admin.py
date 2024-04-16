6ec244dbe38e1d7c4ab0fd53c81963074f3aa7b2
OHMelin <oliverholstmelin@gmail.com>
2024-04-16 14:36:57 +0200

    Add scraper test

--- price_match/admin.py
+++ price_match/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from price_match.models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ["creation_datetime", "total_price"]
+    list_display = ["name", "url", "creation_datetime"]
+
+admin.site.register(Product, ProductAdmin)
--- price_match/models.py
+++ price_match/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=512, null=True, blank=True)
+    url = models.CharField(max_length=512, null=True, blank=True)
+    price = models.FloatField(default=0)
+    ean = models.CharField(max_length=512, null=True, blank=True)
+    color = models.CharField(max_length=512, null=True, blank=True)
+    shipping_cost = models.FloatField(default=0)
+    total_price = models.FloatField(null=True, blank=True)
+    accepted = models.BooleanField(default=False)
+    creation_datetime = models.DateTimeField(auto_now_add=True)
+    acceptance_datetime = models.DateTimeField(null=True, blank=True)
+    product_image = models.ImageField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.total_price = self.price + self.shipping_cost
+        super(Product, self).save(*args, **kwargs)
