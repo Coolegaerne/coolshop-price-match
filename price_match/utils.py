7ccbe9c83583899d08f00e8b84fe9dd08ce35efe
ChristianHolm <chh@createitreal.com>
2024-04-17 10:24:40 +0200

    scrape_html_from_website() works and config template made

--- price_match/utils.py
+++ price_match/utils.py
@@ -14,6 +14,8 @@ def scrape_website(product: Product) -> Product:
 
 def scrape_html_from_website(url: str) -> str:
     my_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36"
+    
+    config_file = __get_config_file(url)
 
     chrome_options = Options()
     chrome_options.headless = True
@@ -22,16 +24,30 @@ def scrape_html_from_website(url: str) -> str:
     driver = webdriver.Chrome(options=chrome_options)
     driver.get(url)
     
+    cookie_selector = config_file.get("cookie_selector")
     wait = WebDriverWait(driver, 10)
-    cookie_accept = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#coiPage-1 > div.coi-banner__page-footer > div.coi-button-group > button.coi-banner__accept")))
+    cookie_accept = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"{cookie_selector}")))
     cookie_accept.click()
     
+    price_selector = config_file.get("price_selector")
     wait = WebDriverWait(driver, 10)
-    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "#site-product-price-stock-buy-container > section > div.site-product-stock-price-buy.d-flex > span > span")))
+    wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, f"{price_selector}")))
 
-    print(driver.page_source)
+    page_source = driver.page_source
     driver.quit()
+    return page_source
 
 
 def get_product_from_html(html: str) -> Product:
     pass
+
+
+def __get_config_file(url: str) -> dict:
+    base_url = url.split("/")[2].removeprefix("www.")
+    file_path = f"price_match/website_configs/{base_url}.json"
+    try:
+        with open(file_path, 'r') as file:
+            return json.load(file)
+    except FileNotFoundError:
+        print(f"Config file for {base_url} not found.")
+        return None
