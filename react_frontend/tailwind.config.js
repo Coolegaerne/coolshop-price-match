c77174c9520f3b249d386e13073a13724dc20723
OHMelin <oliverholstmelin@gmail.com>
2024-04-24 11:25:22 +0200

    Restructure project and setup react

---
+++ react_frontend/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    colors: {
+      primary: '#101010',
+      middle: '#151515',
+      secondary: '#202020',
+    },
+    fontFamily: {
+      'handwriting': ['Dancing Script', 'cursive', 'Poppins', 'sans-serif']
+    },
+  plugins: [],
+  }
+}
