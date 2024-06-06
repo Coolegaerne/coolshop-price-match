4ed26583baaa0bc1d8f574b58ff3e2caa087cd04
MagNsch <magnus02@live.dk>
2024-06-06 12:19:01 +0200

    Parameterized module used to simplify tests for EANTestCases and ExtractPriceFromStringCases
    Added:
    Mockconfig class
    TestGetProductFromHTML

--- django_backend/price_match/tests.py
+++ django_backend/price_match/tests.py
@@ -1,88 +1,93 @@
 from django.test import TestCase
 from .utils import find_ean_in_string, extract_numbers_from_string, get_product_from_html
-from .models import Config, PriceMatch
+from parameterized import parameterized
+from unittest.mock import MagicMock
 
 
-class PriceMatchTestCases(TestCase):
-    def test_ean_found(self):
-        #Arrange 
-        input_string = "This input string contains a valid EAN: 5594962001960."
-        expected_output = "5594962001960"
+class EANTestCases(TestCase):
+    #Arrange
+    @parameterized.expand([
+        ("This input string contains a valid EAN: 5594962001960.", "5594962001960"),
+        ("This string does not contain a valid EAN.", None),
+        ("Test text 5594962001960 and some text", "5594962001960"),
+        ("find First EAN 5323296204450 and second EAN 9876543210987.","5323296204450"),
+        ("Invalid EAN: 12345abcde678 fafsfasf.", None)
+    ]) 
 
+    def test_find_ean_in_string(self, input_string, expected_output):
         #Act
         result = find_ean_in_string(input_string)
-
         #Assert
         self.assertEqual(result, expected_output)
 
 
-    def test_ean_notfound(self):
-        #Arrange
-        input_string = "This string does not contain a valid EAN."
-        expected_output = None
+class ExtractPriceFromStringTestCases(TestCase):
+    #Arrange
+    @parameterized.expand([
+        ("The price is 2,234.56 kr.", "2234.56"),
+        ("No numbers here", "No numbers here")
+    ])
 
-        #Act
-        result = find_ean_in_string(input_string)
-
-        #Assert 
-        self.assertEqual(result, expected_output)
-
-
-    def test_ean_in_middel_of_string(self):
-        #Arrange
-        input_string = "Test text 5594962001960 and some text"
-        expected_output = "5594962001960"
-
-        #Act
-        result = find_ean_in_string(input_string)
-
-        #Assert
-        self.assertEqual(result, expected_output)
-
-
-    def test_multiple_eans_in_string(self):
-        #Arrange
-        input_string = "find First EAN 5323296204450 and second EAN 9876543210987."
-        expected_output = "5323296204450"
-
-        #Act 
-        result = find_ean_in_string(input_string)
-
-        #Assert
-        self.assertEqual(result, expected_output)
-
-    def test_ean_with_non_digits(self):
-        #Arrange
-        input_string = "Invalid EAN: 12345abcde678 fafsfasf."
-        expected_output = None
-
-        #Act
-        result = find_ean_in_string(input_string)
-
-        #Assert
-        self.assertEqual(result, expected_output)
-
-
-    #Tests for method "extract_numbers_from_string"
-    def test_numbers_extracted(self):
-        #Arrange
-        input_string = "The price is $1,234.56"
-        expected_output = "1234.56"
-
+    def test_numbers_extracted(self, input_string, expected_output):
         #Act
         result = extract_numbers_from_string(input_string)
-
         #Assert
         self.assertEqual(result, expected_output)
 
 
-    def test_no_numbers(self):
+class MockConfig:
+    base_url = "proshop.dk"
+    slowest_element_selector = ".site-currency-attention"
+    cookie_selector = "#coiPage-1 > div.coi-banner__page-footer > div.coi-button-group > button.coi-banner__accept"
+    specification_selector = ""
+    price_selector = ".site-currency-attention"
+    name_selector = "#site-product-price-stock-buy-container > h1"
+    stock_status_selector = ".site-stock > .status"
+    shipping_price_selector = ".site-stock > .shipping"
+    currency = "dkk"
+
+
+class TestGetProductFromHTML(TestCase):
+    def test_get_product_from_html(self):
+        
         #Arrange
-        input_string = "No numbers here"
-        expected_output = "No numbers here"
+        mock_config = MockConfig()
+        #Simulate an HTML page
+        mock_html = """ 
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Proshop Product</title>
+            </head>
+            <body>
+                <div id="site-product-price-stock-buy-container">
+                    <h1>Product Title</h1>
+                    <div class="site-stock">
+                        <div class="shipping">Shipping: 20 DKK</div>
+                        <div class="status">In Stock</div>
+                        <div>Other Info</div>
+                    </div>
+                </div>
+                <div class="site-currency-attention">Product Price: 500 DKK</div>
+                <div class="coi-banner__page-footer">
+                    <div class="coi-button-group">
+                        <button class="coi-banner__accept">Accept Cookies</button>
+                    </div>
+                </div>
+            </body>
+            </html>
+            """
+        mock_price_match = MagicMock()
+        mock_binary_screenshot = b'mocked_screenshot'
 
         #Act
-        result = extract_numbers_from_string(input_string)
+        result = get_product_from_html(mock_config, mock_html, mock_price_match, mock_binary_screenshot)
 
         #Assert
-        self.assertEqual(result, expected_output)
+        self.assertEqual(result, mock_price_match) 
+        self.assertEqual(mock_price_match.price, "500")  
+        self.assertEqual(mock_price_match.shipping_price, "20")
+        self.assertEqual(mock_price_match.name, "Product Title")
+        self.assertEqual(mock_price_match.stock_status, "In Stock")
